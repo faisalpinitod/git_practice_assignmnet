b2aa6bd2f216069658651ec566776f2f3cec94f5
faisalpinitod <faisalpinitod@gmail.com>
2022-09-17 21:40:39 +0530

    prime and pallindrome

---
+++ palindrome_problem.js
@@ -0,0 +1,10 @@
+var str="naman";
+var bag="";
+for(i=0;i<str.length;i++){
+	bag+=str[i];
+}
+if(str==bag){
+	console.log(True);
+}else{
+	console.log(false);
+}
---
+++ prime_problem.js
@@ -0,0 +1,12 @@
+var a=13;
+var flag=0;
+for(i=0;i<a;i++){
+    if(a%i==0){
+        flag++;
+    }
+}
+if(flag==1){
+    console.log("palindrome");
+}else{
+    console.log("palindrome");
+}
